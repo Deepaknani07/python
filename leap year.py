4f191500a690614fbbbed84e4e10b42e218f165f
Deepaknani07 <121151296+Deepaknani07@users.noreply.github.com>
2024-01-16 19:34:36 +0530

    Create leap year.py

---
+++ leap year.py
@@ -0,0 +1,8 @@
+def checkyear(year):
+    return (((year% 4 ==0) and (year% 100 !=0)) or (year% 400 ==0))
+year=2000
+if(checkyear(year)):
+
+    print('leap year')
+else:
+    print("not a leap year")
