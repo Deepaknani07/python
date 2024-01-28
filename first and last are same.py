c28c3f07aea0c0ee514effd3a8d8043a600c6dcf
Deepaknani07 <121151296+Deepaknani07@users.noreply.github.com>
2024-01-28 19:13:51 +0530

    Create first and last are same.py

---
+++ first and last are same.py
@@ -0,0 +1,10 @@
+numbers_x =[10,20,30,40,10]
+def first_last(numbers_x):
+    first=numbers_x[0]
+    last=numbers_x[-1]
+    if first==last:
+        return True
+    else:
+        return False
+print(first_last(numbers_x))    
+    
