05d7627aa26f2e148d7317189252f806ad3d1c7e
Deepaknani07 <121151296+Deepaknani07@users.noreply.github.com>
2024-01-28 19:02:10 +0530

    Create splitstring with multiple delimiters.py

---
+++ splitstring with multiple delimiters.py
@@ -0,0 +1,10 @@
+import re
+text ='the quick brown\nfox jumps*over the lazy dog.'
+print(re.split('; |, |\*|\n',text))
+
+
+u can try the both the methods for it
+
+
+d="the,book,is.good"
+print(d.split(','))
