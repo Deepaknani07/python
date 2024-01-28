114de86b9418e0ec5ca52fc0790d424e2876d4c9
Deepaknani07 <121151296+Deepaknani07@users.noreply.github.com>
2024-01-28 20:04:57 +0530

    Create to show length of the string.py

---
+++ to show length of the string.py
@@ -0,0 +1,15 @@
+t="subscribe the channel"
+print(len(t))
+
+
+
+with built in functions
+
+t="subscribe the channel"
+
+def string_length(str1):
+    count=0
+    for char in str1:
+        count+=1
+    return count
+print(string_length(t))    
